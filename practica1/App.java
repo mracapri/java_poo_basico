f48bad19d6accf872d214e3b23e51d60153846b9
Mario Rivera Angeles <mra_capri@msn.com>
2013-06-11 10:21:37 -0500

    Practica1, 2 y 3 para empezar curso

---
+++ practica1/App.java
@@ -0,0 +1,11 @@
+public class App{
+	
+	void hagoAlgo(){
+		System.out.println("Hola mundo!");		
+	}
+
+	public static void main(String[] arg){
+		App app = new App();
+		app.hagoAlgo();
+	}
+}
---
+++ practica2/src/main/java/com/curso/App.java
@@ -0,0 +1,12 @@
+package com.curso;
+
+public class App{
+	void hagoAlgo(){
+		System.out.println("Hola mundo!");		
+	}
+
+	public static void main(String[] arg){
+		App app = new App();
+		app.hagoAlgo();
+	}
+}
