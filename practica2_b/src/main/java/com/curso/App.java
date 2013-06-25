0b25ccbbb555b3215c31b1f364c4ebe4d7983246
Mario Rivera Angeles <mra_capri@msn.com>
2013-06-24 21:23:15 -0500

    Constructores

---
+++ practica2_b/src/main/java/com/curso/App.java
@@ -0,0 +1,14 @@
+package com.curso;
+
+public class App{
+
+	public static void main(String[] arg){
+		
+		Robot r2d2 = null;
+		r2d2 = new Robot();
+		r2d2 = new Robot("wally");
+		r2d2 = new Robot(new SistemaOperativo2());
+		r2d2 = new Robot("eva", new SistemaOperativo2());
+	
+	}
+}
