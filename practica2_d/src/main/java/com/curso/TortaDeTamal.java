ce909c35b2d76bee44215741c63aa417993848e2
mracapri <mracapri@UL-TRON.(none)>
2013-06-27 11:15:46 -0500

    Constructores Torta de tamal

---
+++ practica2_d/src/main/java/com/curso/TortaDeTamal.java
@@ -0,0 +1,11 @@
+package com.curso;
+
+public class TortaDeTamal{
+	private Tamal tamal;
+	private Pan pan;
+
+	public TortaDeTamal(Tamal tamal, Pan pan){
+		this.tamal = tamal;
+		this.pan = pan;
+	}
+}
